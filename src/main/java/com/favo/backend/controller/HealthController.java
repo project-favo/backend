b7ef438dd72413ce90f308eb9e1ded56fb94bea4
atilaydogdu <atilaatilay@gmail.com>
2025-12-06 21:34:16 +0300

    Update health endpoint response

--- src/main/java/com/favo/backend/controller/HealthController.java
+++ src/main/java/com/favo/backend/controller/HealthController.java
@@ -8,6 +8,6 @@ public class HealthController {
 
     @GetMapping("/api/health")
     public String health() {
-        return "Backend is running!";
+        return "Backend is running!!";
     }
 }
