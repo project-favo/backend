12bf9c2b20ed75c8bb086d350688bfc60376f8f8
AliEkinBozay <154544250+AliEkinBozay@users.noreply.github.com>
2025-12-12 00:07:34 +0300

    BaseEntity
    Base Entity Class

--- src/main/java/com/favo/backend/Domain/Common/BaseEntity.java
+++ src/main/java/com/favo/backend/Domain/Common/BaseEntity.java
@@ -1,4 +1,17 @@
 package com.favo.backend.Domain.Common;
 
-public class BaseEntity {
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class  BaseEntity  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    protected LocalDateTime createdAt;
 }
